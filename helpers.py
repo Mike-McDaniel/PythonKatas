cc19a1241d24e096733162c0dc280cbb6893dcc7
Mike McDaniel <truemedicalgamer@gmail.com>
2023-07-06 16:21:11 -0700

    Day 2, 20 lines!

--- helpers.py
+++ helpers.py
@@ -1,2 +1,11 @@
 def greeting(username): # github test 2
     return 'hello ' +         username +'!'
+
+def adder(x,y,z):
+    return x+y+z
+
+def add10(x):
+    return x+10
+
+def square(x):
+    return x**2
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from helpers import greeting
+from helpers import add10, adder, greeting, square
 
 class TestStringMethods(unittest.TestCase):
 
@@ -15,6 +15,16 @@ class TestStringMethods(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(greeting('steve'), 'hello steve!')
         self.assertEqual(greeting('dave'), 'hello dave!')
+
+    def test_adder(self):
+        self.assertEqual(adder(1,2,3), 6)
+        self.assertEqual(adder(4,5,1), 10)
+
+    def test_add10(self):
+        self.assertEqual(add10(5), 15)
+
+    def test_square(self):
+        self.assertEqual(square(5), 25)
 
 if __name__ == '__main__':
     unittest.main()
