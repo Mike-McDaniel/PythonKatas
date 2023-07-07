4cff9f66c307b4883d74350e4fe06005a7bd2451
Mike McDaniel <truemedicalgamer@gmail.com>
2023-07-07 12:26:06 -0700

    Improved add function and 2 new square functions with tests.

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 def greeting(username): # github test 2
     return 'hello ' +         username +'!'
 
-def adder(x,y,z):
+def add(x=0,y=0,z=0):
     return x+y+z
 
 def add10(x):
@@ -9,3 +9,10 @@ def add10(x):
 
 def square(x):
     return x**2
+
+def sum_of_squares(x):
+    return add(square(x), square(x))
+
+def sum_of_squares2(x):
+    square_of_x=square(x)
+    return add(square_of_x, square_of_x)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from helpers import add10, adder, greeting, square
+from helpers import add10, add, greeting, square, sum_of_squares, sum_of_squares2
 
 class TestStringMethods(unittest.TestCase):
 
@@ -16,15 +16,24 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(greeting('steve'), 'hello steve!')
         self.assertEqual(greeting('dave'), 'hello dave!')
 
-    def test_adder(self):
-        self.assertEqual(adder(1,2,3), 6)
-        self.assertEqual(adder(4,5,1), 10)
+    def test_add(self):
+        self.assertEqual(add(1,2,3), 6)
+        self.assertEqual(add(4,5,1), 10)
+        self.assertEqual(add(1,1), 2)
+        self.assertEqual(add(1), 1)
+        self.assertEqual(add(), 0)
 
     def test_add10(self):
         self.assertEqual(add10(5), 15)
 
     def test_square(self):
         self.assertEqual(square(5), 25)
+
+    def test_sum_of_squares(self):
+        self.assertEqual(sum_of_squares(3), 18)
+
+    def test_sum_of_squares2(self):
+        self.assertEqual(sum_of_squares2(3), 18)
 
 if __name__ == '__main__':
     unittest.main()
