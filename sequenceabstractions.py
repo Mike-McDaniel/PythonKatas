3f382943a20f8f8ff1082782fa9801dc257458e9
Mike McDaniel <truemedicalgamer@gmail.com>
2023-07-13 10:05:51 -0700

    sequence abstraction practice.

--- sequenceabstractions.py
+++ sequenceabstractions.py
@@ -4,27 +4,38 @@
 # transforming a list
 # "mapping" a list
 #       map = output list is always the same length as the input list
-def addTenToAll(numbers) :
-	newNumbers = []
+# def addTenToAll(numbers):
+# 	newNumbers = []
+# 	for x in numbers:
+# 		newNumbers.append(x + 10)
+# 	return newNumbers
+
+def addTenToAll(numbers):
+	new_numbers = []
 	for x in numbers:
-		newNumbers.append(x + 10)
-	return newNumbers
+		new_numbers.append(x + 10)
+	return new_numbers
+
+
+# def firstLetters(names):
+# 	newNames = []
+# 	for name in names:
+# 		newNames.append(name[0])
+# 	return newNames
 
 def firstLetters(names):
-	newNames = []
-	for name in names:
-		newNames.append(name[0])
-	return newNames
+	new_names = []
+	for letter in names:
+		new_names.append(letter[0])
+	return new_names
 
 def oddOrEven(numbers):
 	newNumbers = []
 	for number in numbers:
 		if (number % 2) == 0:
 			newNumbers.append("even")
-			# print("even")
 		else:
 			newNumbers.append("odd")
-			# print("odd")
 	return newNumbers
 
 def oddOrPlus10(numbers):
