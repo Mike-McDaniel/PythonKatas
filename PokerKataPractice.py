38305397c27538616a071cb468f94c697885e010
Mike McDaniel <truemedicalgamer@gmail.com>
2023-07-31 18:50:35 -0700

    PokerKataPractice

---
+++ PokerKataPractice.py
@@ -0,0 +1,42 @@
+def get_face(card):
+    card_dictionary_face = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14
+    }
+    return card_dictionary_face[card[0]]
+
+def get_suit(card):
+    card_dictionary_suit = {
+        "C": "Clubs",
+        "D": "Diamonds",
+        "H": "Hearts",
+        "S": "Spades"
+    }
+    return card_dictionary_suit[card[1]]
+
+def get_card(card):
+    card_dictionary = {
+        "Face": get_face(card),
+        "Suit": get_suit(card)
+    }
+    return card_dictionary
+
+def get_hand(cards):
+    return list(map(lambda card: get_card(card), cards))
+
+def parse_hand_string(cards_string):
+    return cards_string.split(" ")
+
+def parse_hand(hand):
+    return get_hand(parse_hand_string(hand))
--- PokerKataPractice_test.py
+++ PokerKataPractice_test.py
@@ -1,77 +1,41 @@
 import unittest
 
-from PokerKataPractice import get_face
+from PokerKataPractice import get_card, get_face, get_hand, get_suit, parse_hand, parse_hand_string
 
 class TestPokerKataPractice(unittest.TestCase):
 
 
     
     def test_get_face(self):
-        self.assertEqual(get_face("2H"), 2)
-    
+        self.assertEqual(get_face("2X"), 2)
+        self.assertEqual(get_face("KX"), 13)
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # def test_get_suit(self):
-    #     self.assertEqual(get_suit("6C"), "clubs")
-    #     self.assertEqual(get_suit("4D"), "diamonds")
-    #     self.assertEqual(get_suit("2H"), "hearts")
-    #     self.assertEqual(get_suit("QS"), "spades")
+    def test_get_suit(self):
+        self.assertEqual(get_suit("XC"), "Clubs")
+        self.assertEqual(get_suit("XD"), "Diamonds")
 
-    # def test_get_card(self):
-    #     card1 = get_card("6C")
-    #     self.assertEqual(card1["suit"], "clubs")
-    #     self.assertEqual(card1["face"], 6)
-    #     card2 = get_card("4D")
-    #     self.assertEqual(card2["suit"], "diamonds")
-    #     self.assertEqual(card2["face"], 4)
-    #     self.assertEqual(get_card("2H")["suit"], "hearts")
-    #     self.assertEqual(get_card("QS")["suit"], "spades")
+    def test_get_card(self):
+        self.assertEqual(get_card("2S")["Face"], 2)
+        self.assertEqual(get_card("2S")["Suit"], "Spades")
+    
+    def test_get_hand(self):
+        card = get_hand(["JC", "4D"])
+        self.assertEqual(get_hand(["JC", "4D"])[0]["Face"], 11)
+        self.assertEqual(card[0]["Face"], 11)
+        self.assertEqual(get_hand(["JC", "4D"])[1]["Suit"], "Diamonds")
+        self.assertEqual(card[1]["Suit"], "Diamonds")
+    
+    def test_parse_hand_string(self):
+        self.assertEqual(parse_hand_string("XX XX"), ["XX", "XX"])
+        self.assertEqual(parse_hand_string("XX XX XX XX XX"), ["XX", "XX", "XX", "XX", "XX"])
 
-    # def test_get_hand(self):
-    #     self.assertEqual(get_hand([]), [])
-    #     hand = get_hand(["6C", "4D"])
-    #     self.assertEqual(hand[0]["suit"], "clubs")
-    #     self.assertEqual(hand[0]["face"], 6)
-    #     self.assertEqual(hand[1]["suit"], "diamonds")
-    #     self.assertEqual(hand[1]["face"], 4)
-
-    # def test_parse_hand_string(self):
-    #     self.assertEqual(parse_hand_string("6C"), ["6C"])
-    #     self.assertEqual(parse_hand_string("6C 4D"), ["6C", "4D"])
-
-    # def test_parse_hand(self):
-    #     hand = parse_hand("6C 4D")
-    #     self.assertEqual(hand[0]["suit"], "clubs")
-    #     self.assertEqual(hand[0]["face"], 6)
-    #     self.assertEqual(hand[1]["suit"], "diamonds")
-    #     self.assertEqual(hand[1]["face"], 4)
+    def test_parse_hand(self):
+        hand = parse_hand("6C 4D")
+        self.assertEqual(hand[0]["Suit"], "Clubs")
+        self.assertEqual(hand[0]["Face"], 6)
+        self.assertEqual(hand[1]["Suit"], "Diamonds")
+        self.assertEqual(hand[1]["Face"], 4)
+    
     
 
 if __name__ == '__main__':
